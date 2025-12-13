01fb34166eb9182faf116a1809f0cc35a2bc0c96
Alejandro Barraza <abarraza.pk@gmail.com>
2025-12-13 20:08:23 -0300

    fix: corregir admin de LoginAttempt
    - Cambiar campo 'timestamp' a 'attempted_at' (nombre correcto del campo)
    - Configurar correctamente list_display, list_filter y date_hierarchy
    - Deshabilitar add y change permissions (solo lectura)

--- autenticacion/admin.py
+++ autenticacion/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import LoginAttempt
 
-# Register your models here.
+
+@admin.register(LoginAttempt)
+class LoginAttemptAdmin(admin.ModelAdmin):
+    list_display = ('username', 'ip_address', 'successful', 'attempted_at')
+    list_filter = ('successful', 'attempted_at')
+    search_fields = ('username', 'ip_address')
+    readonly_fields = ('username', 'ip_address', 'successful', 'attempted_at')
+    date_hierarchy = 'attempted_at'
+    
+    def has_add_permission(self, request):
+        return False
+    
+    def has_change_permission(self, request, obj=None):
+        return False
